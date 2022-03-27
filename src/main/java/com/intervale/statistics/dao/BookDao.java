1cad29195732ab181d464279a4a2d842e43e6ca4
TrueToneBY <pasblek@yandex.ru>
2022-03-27 18:34:26 +0300

    JavaDoc_

--- src/main/java/com/intervale/statistics/dao/BookDao.java
+++ src/main/java/com/intervale/statistics/dao/BookDao.java
@@ -4,6 +4,9 @@ import com.intervale.statistics.model.Book;
 
 import java.util.List;
 
+/**
+ * interface
+ */
 public interface BookDao {
 
     String selectBookByTitle = "select * from book where title ilike ? limit 1";
